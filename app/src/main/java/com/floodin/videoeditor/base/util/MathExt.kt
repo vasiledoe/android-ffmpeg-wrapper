d400802dd338a675ad85948081ca82c1a5736449
Vasile Racovita <vasile.doe1992@gmail.com>
2022-08-01 15:58:43 +0300

    log details

--- app/src/main/java/com/floodin/videoeditor/base/util/MathExt.kt
+++ app/src/main/java/com/floodin/videoeditor/base/util/MathExt.kt
@@ -3,6 +3,7 @@ package com.floodin.videoeditor.base.util
 import android.util.DisplayMetrics
 import android.util.TypedValue
 import java.text.DecimalFormat
+import java.util.concurrent.TimeUnit
 import kotlin.math.ceil
 import kotlin.math.log10
 import kotlin.math.pow
@@ -16,6 +17,20 @@ fun Long.toPrettyFileSize(): String {
     val units = arrayOf("B", "KB", "MB", "GB", "TB")
     val digitGroups = (log10(toDouble()) / log10(1024.0)).toInt()
     return DecimalFormat("#.##").format(this / 1024.0.pow(digitGroups.toDouble())) + " " + units[digitGroups]
+}
+
+fun Long.toPrettyTimeElapsed(): String {
+    if (this <= 0) return "0 sec"
+    return String.format(
+        "%02d : %02d : %02d",
+        TimeUnit.MILLISECONDS.toHours(this),
+        TimeUnit.MILLISECONDS.toMinutes(this) - TimeUnit.HOURS.toMinutes(
+            TimeUnit.MILLISECONDS.toHours(this)
+        ),
+        TimeUnit.MILLISECONDS.toSeconds(this) - TimeUnit.MINUTES.toSeconds(
+            TimeUnit.MILLISECONDS.toMinutes(this)
+        )
+    )
 }
 
 fun Float.toPx(displayMetrics: DisplayMetrics): Int = TypedValue.applyDimension(
